7df6669b009d38c5e3c86c1c45f50d6ce62b2785
Bekk Blando <bekkblando@gmail.com>
2015-07-21 20:57:33 -0400

    Cleaned Up Code

--- random_art.py
+++ random_art.py
@@ -13,16 +13,16 @@ def create_expression():
     w = random.randint(1, 30)
     s = random.randint(1, 5)
     choice = random.randint(1, 3)
+
     def expr(x, y):
-        for item in range(1):
+        for item in range(3):
             x = x * i
-            # x = x/w
             y = y * i
             x += m.factorial(s)
 
-            op1 = (lambda x, y: x*i*y)(y, x)
-            op2 = (lambda x, y: (x*w)%s + i)(x, y)
-            op3 = (lambda x, y: ((m.acos(y**2) *m.gamma(x))))(x, y)
+            op1 = (lambda x, y: x*i*y/m.sin(299792458)/3.14345353)(y, x)
+            op2 = (lambda x, y: (x*w/3.14) % s + i)(x, y)
+            op3 = (lambda x, y: (m.cos(y**2) * m.gamma(x)))(x, y)
             if choice == 1:
                 op = op1
                 ops = op2
@@ -32,12 +32,10 @@ def create_expression():
             if choice == 3:
                 op = op3
                 ops = op1
-            # print(len(range((lambda x, y: (x**4) * (y**2))(random.choice(choices), random.choice(choices)))))
             u = (lambda x, y: (x*2) * (y*2))(op, ops)
             ans = u
         return ans
 
-        # return x * (1/m.sqrt(1 - ((y)/299792458**2)))
     return expr
 
 
